1637fe4408eab3d6580c6d3b99dbe3caeb461cf3
afnan <afnan@elmeasure.com>
2022-11-08 20:56:12 +0530

    fetch

--- library/models.py
+++ library/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+# from django.db import models
+from djongo import models
+
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.username, self.password
+
--- library/views.py
+++ library/views.py
@@ -1,4 +1,13 @@
+import json
+from collections import ChainMap
 from django.shortcuts import render
+from django.http import JsonResponse
+from pymongo import MongoClient
+from configurations import AppConfig
+
+Configs = AppConfig()
+client = MongoClient(Configs.getMongoUrl())
+db = client.library
 
 
 def login(request):
@@ -10,7 +19,19 @@ def books(request):
 
 
 def home(request):
-    return render(request, "admin.home.html")
+    if request.method == "POST":
+        req = json.loads(request.body)
+        userName = req['userDetails'].get("username")
+        password = req['userDetails'].get("password")
+        userSearch = [user for user in db.users.find({"username": userName, "isdeleted": {'$nin': ["true", True]}})]
+        credentials = dict(ChainMap(*userSearch)) if len(userSearch) > 0 else False
+        if credentials is not False and credentials['username'] == userName and credentials['password'] == password:
+            return JsonResponse({"message": "hello", "status": "success"})
+            # return render(request, "admin.home.html")
+        else:
+            return JsonResponse({"message": "invalid credentials"})
+    else:
+        return JsonResponse({"message": "invalid credentials"})
 
 
 def rent(request):
